1688675faf3553b51df788394ebb0da21748f314
Kerry hHampton <65149075+jessekerry@users.noreply.github.com>
2021-02-21 22:48:34 -0800

    week 1 commit

---
+++ week1/todo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+    return(
+        <div>
+            <p>Honey Do List</p>
+            <input type="checkbox" />Take The Trash To The Street<br/>
+            <input type="checkbox" />Turn In The Recyclables<br/>
+            <input type="checkbox" />Put Gas In The Truck<br/>
+        </div>
+    )
+}
+
+export default App
